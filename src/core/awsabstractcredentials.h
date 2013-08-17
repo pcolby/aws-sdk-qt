4159307c32ac512dce09ee22897f5b8ffeb8c62a
Paul Colby <git@colby.id.au>
2013-08-17 22:25:19 +0000

    Thoughts on refreshability.

--- src/core/awsabstractcredentials.h
+++ src/core/awsabstractcredentials.h
@@ -15,6 +15,14 @@ class QTAWS_EXPORT AwsAbstractCredentials : public QObject {
 public:
     virtual QString accessKeyId() const = 0;
     virtual QString secretKey() const = 0;
+    virtual QString token() const = 0;
+
+    QDateTime expiration() const;
+    virtual bool isRefreshable() const = 0;
+    virtual bool isExpired() const = 0;
+
+public slots:
+    virtual bool refresh() = 0;
 
 signals:
     void changed();
