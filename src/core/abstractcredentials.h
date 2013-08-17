9ba7c9088ae985e34fe1c891ee9cc499b7b66348
Paul Colby <git@colby.id.au>
2013-08-17 07:21:48 +0000

    More credentials work.

--- src/core/abstractcredentials.h
+++ src/core/abstractcredentials.h
@@ -9,26 +9,17 @@
 #define QTAWS_EXPORT Q_DECL_IMPORT
 #endif
 
-//namespace aws {
-//namespace core {
-
 class QTAWS_EXPORT AbstractCredentials : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QString accessKeyId READ accessKeyId WRITE setAccessKeyId NOTIFY accessKeyIdChanged)
-    Q_PROPERTY(QString secretKey   READ secretKey   WRITE setSecretKey   NOTIFY secretKeyChanged)
 
 public:
     virtual QString accessKeyId() const = 0;
     virtual QString secretKey() const = 0;
-    virtual void setAccessKeyId(const QString &accessKeyId) = 0;
-    virtual void setSecretKey(const QString &setSecretKey) = 0;
 
 signals:
-    void accessKeyIdChanged(const QString &accessKeyId);
-    void secretKeyChanged(const QString &secretKey);
-};
+    void changed();
 
-//} }
+};
 
 #endif
--- src/core/credentials.h
+++ src/core/credentials.h
@@ -9,9 +9,6 @@
 #define QTAWS_EXPORT Q_DECL_IMPORT
 #endif
 
-//namespace aws {
-//namespace core {
-
 class CredentialsPrivate;
 
 class QTAWS_EXPORT Credentials : public AbstractCredentials
@@ -19,15 +16,17 @@ class QTAWS_EXPORT Credentials : public AbstractCredentials
     Q_OBJECT
 
 public:
+    Credentials(const QString &accessKeyid, const QString &secretKey);
+
     virtual QString accessKeyId() const;
     virtual QString secretKey() const;
     virtual void setAccessKeyId(const QString &accessKeyId);
     virtual void setSecretKey(const QString &secretKey);
 
 private:
+    CredentialsPrivate * const d_ptr;
     Q_DECLARE_PRIVATE(Credentials);
-};
 
-//} }
+};
 
 #endif
