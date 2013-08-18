498419a965516b4d48ff409ae9e72bd5eb1cd7e5
Paul Colby <git@colby.id.au>
2013-08-18 05:36:24 +0000

    More documentation.

--- src/core/awsabstractcredentials.h
+++ src/core/awsabstractcredentials.h
@@ -16,6 +16,10 @@ public:
     virtual QString secretKey() const = 0;
     virtual QString token() const = 0;
 
+    // Support for refreshable credentials.  Note, we could use meta-data inspection
+    // here, such as QObject::inherits and QObject::qobject_cast, but isRefreshable()
+    // is likely to be called before every service call, so performance is especially
+    // important here
     virtual QDateTime expiration() const;
     virtual bool isExpired() const;
     virtual bool isRefreshable() const;
