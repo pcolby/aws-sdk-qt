3ba46057d4705b0fb99786b8c03ffa8030667af4
Paul Colby <git@colby.id.au>
2013-08-20 09:52:06 +0000

    Updated signature tests for recent API changes (no real tests yet though).

--- test/core/testawsabstractsignature.cpp
+++ test/core/testawsabstractsignature.cpp
@@ -3,16 +3,15 @@
 #include "../../src/core/awsabstractsignature.h"
 #include "../../src/core/awsbasiccredentials.h"
 
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkRequest>
+//#include <QtNetwork/QNetworkAccessManager>
+//#include <QtNetwork/QNetworkRequest>
 
 Q_DECLARE_METATYPE(QNetworkAccessManager::Operation)
 
 // Bare minimum concrete mock class.
 class MockSignature : public AwsAbstractSignature {
-public:
-    MockSignature(QObject * const parent = 0) : AwsAbstractSignature(parent) { }
 
+public:
     virtual void sign(const QNetworkAccessManager::Operation operation,
                       QNetworkRequest &request,
                       const AwsAbstractCredentials &credentials,
--- test/core/testawssignaturev2.cpp
+++ test/core/testawssignaturev2.cpp
@@ -1,21 +1,44 @@
 #include "testawssignaturev2.h"
 
+#include "../../src/core/awsbasiccredentials.h"
 #include "../../src/core/awssignaturev2.h"
 
-#include <QtNetwork/QNetworkRequest>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+
+Q_DECLARE_METATYPE(QNetworkAccessManager::Operation)
 
 void TestAwsSignatureV2::sign_data() {
+    QTest::addColumn<QNetworkAccessManager::Operation>("operation");
     QTest::addColumn<QNetworkRequest>("request");
+    QTest::addColumn<QString>("accessKeyId");
+    QTest::addColumn<QString>("secretKey");
+    QTest::addColumn<QString>("securityToken");
     QTest::addColumn<QByteArray>("data");
-    //QTest::addColumn<AwsBasicCredentials>("credentials");
+    QTest::addColumn<QNetworkRequest>("expected");
 
-    QTest::newRow("null") << QNetworkRequest() << QByteArray();
+    QTest::newRow("null") << QNetworkAccessManager::GetOperation << QNetworkRequest()
+                          << QString("") << QString("") << QString("")
+                          << QByteArray() << QNetworkRequest();
 }
 
 void TestAwsSignatureV2::sign() {
+    QFETCH(QNetworkAccessManager::Operation, operation);
     QFETCH(QNetworkRequest, request);
+    QFETCH(QString, accessKeyId);
+    QFETCH(QString, secretKey);
+    QFETCH(QString, securityToken);
     QFETCH(QByteArray, data);
-    //QFETCH(QString, credentials);
+    QFETCH(QNetworkRequest, expected);
 
-    QVERIFY(true); ///< @todo
+    const AwsBasicCredentials credentials(accessKeyId, secretKey, securityToken);
+
+    AwsSignatureV2 signature;
+    signature.sign(operation, request, credentials, data);
+    QCOMPARE(request, expected);
+
+    QBENCHMARK {
+        QFETCH(QNetworkRequest, request);
+        signature.sign(operation, request, credentials, data);
+    }
 }
