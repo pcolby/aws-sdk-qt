29ac8964fef5b93f8479ab930c8e91680e454384
Paul Colby <git@colby.id.au>
2013-08-24 12:33:40 +0000

    Moved a functon for consitency.

--- test/core/testawssignaturev4.cpp
+++ test/core/testawssignaturev4.cpp
@@ -193,42 +193,6 @@ void TestAwsSignatureV4::canonicalRequest()
 
 }
 
-void TestAwsSignatureV4::TestAwsSignatureV4::stringToSign_data()
-{
-    QTest::addColumn<QByteArray>("algorithmDesignation");
-    QTest::addColumn<QDateTime> ("requestDate");
-    QTest::addColumn<QByteArray>("credentialScope");
-    QTest::addColumn<QByteArray>("canonicalRequest");
-    QTest::addColumn<QByteArray>("expected");
-
-    QTest::newRow("null")
-        << QByteArray()
-        << QDateTime()
-        << QByteArray()
-        << QByteArray()
-        << QByteArray("\n\n\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
-
-    // Example from http://docs.aws.amazon.com/general/latest/gr/sigv4-create-string-to-sign.html
-    QTest::newRow("official")
-        << QByteArray("AWS4-HMAC-SHA256")
-        << QDateTime::fromString("20110909T233600Z", "yyyyMMddThhmmssZ")
-        << QByteArray("20110909/us-east-1/iam/aws4_request")
-        << QByteArray(
-            "POST\n"
-            "/\n"
-            "\n"
-            "content-type:application/x-www-form-urlencoded; charset=utf-8\n"
-            "host:iam.amazonaws.com\n"
-            "x-amz-date:20110909T233600Z\n"
-            "\n"
-            "content-type;host;x-amz-date\n"
-            "b6359072c78d70ebee1e81adcbab4f01bf2c23245fa365ef83fe8f1f955085e2")
-        << QByteArray("AWS4-HMAC-SHA256\n"
-            "20110909T233600Z\n"
-            "20110909/us-east-1/iam/aws4_request\n"
-            "3511de7e95d28ecd39e9513b642aee07e54f4941150d8df8bf94b328ef7e55e2");
-}
-
 void TestAwsSignatureV4::credentialScope_data()
 {
     QTest::addColumn<QDate>("date");
@@ -290,6 +254,42 @@ void TestAwsSignatureV4::signingKey()
     AwsSignatureV4Private signature(QCryptographicHash::Sha256, NULL);
     const QByteArray signingKey = signature.signingKey(credentials, date, region, service);
     QCOMPARE(signingKey, expected);
+}
+
+void TestAwsSignatureV4::TestAwsSignatureV4::stringToSign_data()
+{
+    QTest::addColumn<QByteArray>("algorithmDesignation");
+    QTest::addColumn<QDateTime> ("requestDate");
+    QTest::addColumn<QByteArray>("credentialScope");
+    QTest::addColumn<QByteArray>("canonicalRequest");
+    QTest::addColumn<QByteArray>("expected");
+
+    QTest::newRow("null")
+        << QByteArray()
+        << QDateTime()
+        << QByteArray()
+        << QByteArray()
+        << QByteArray("\n\n\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+
+    // Example from http://docs.aws.amazon.com/general/latest/gr/sigv4-create-string-to-sign.html
+    QTest::newRow("official")
+        << QByteArray("AWS4-HMAC-SHA256")
+        << QDateTime::fromString("20110909T233600Z", "yyyyMMddThhmmssZ")
+        << QByteArray("20110909/us-east-1/iam/aws4_request")
+        << QByteArray(
+            "POST\n"
+            "/\n"
+            "\n"
+            "content-type:application/x-www-form-urlencoded; charset=utf-8\n"
+            "host:iam.amazonaws.com\n"
+            "x-amz-date:20110909T233600Z\n"
+            "\n"
+            "content-type;host;x-amz-date\n"
+            "b6359072c78d70ebee1e81adcbab4f01bf2c23245fa365ef83fe8f1f955085e2")
+        << QByteArray("AWS4-HMAC-SHA256\n"
+            "20110909T233600Z\n"
+            "20110909/us-east-1/iam/aws4_request\n"
+            "3511de7e95d28ecd39e9513b642aee07e54f4941150d8df8bf94b328ef7e55e2");
 }
 
 void TestAwsSignatureV4::stringToSign()
