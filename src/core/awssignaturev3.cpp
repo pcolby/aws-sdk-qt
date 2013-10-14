7e9eebaf52dd3b736695d79c7d035277f7ee077d
Paul Colby <git@colby.id.au>
2013-10-14 18:57:07 +1100

    Capitalised some instanes of "libqtaws" to adhere to the rules of English.

--- src/core/awssignaturev3.cpp
+++ src/core/awssignaturev3.cpp
@@ -3,12 +3,12 @@
 
     This file is part of libqtaws.
 
-    libqtaws is free software: you can redistribute it and/or modify
+    Libqtaws is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    libqtaws is distributed in the hope that it will be useful,
+    Libqtaws is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
