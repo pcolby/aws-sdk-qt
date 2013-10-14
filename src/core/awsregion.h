c8a58463b1a8e3d5ac5246f5db13ab7b2a446f36
Paul Colby <git@colby.id.au>
2013-10-14 18:54:28 +1100

    Updated a "Foobar" reference from the GPL template to correctly specify "libqtaws" instead.

--- src/core/awsregion.h
+++ src/core/awsregion.h
@@ -14,7 +14,7 @@
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with libqtaws.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef AWSREGION_H
