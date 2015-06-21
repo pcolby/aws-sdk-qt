0c72f51493c7278963be561bf7e173bd5a40b5cb
Paul Colby <git@colby.id.au>
2015-06-22 09:06:22 +1000

    Fix a build issue
    Was introduced by the cherry-picking of commit
    a55506a8c86e36ad86c66ae26b6b3125afeb8df7.

--- src/core/awsabstractclient.cpp
+++ src/core/awsabstractclient.cpp
@@ -212,8 +212,7 @@ void AwsAbstractClient::requestFinished(QObject * object)
  * @param  q  Pointer to this object's public AwsAbstractClient instance.
  */
 AwsAbstractClientPrivate::AwsAbstractClientPrivate(AwsAbstractClient * const q)
-    : credentials(NULL), endpoint(NULL), networkAccessManager(NULL),
-      signature(NULL), q_ptr(q)
+    : credentials(NULL), networkAccessManager(NULL), signature(NULL), q_ptr(q)
 {
 
 }
