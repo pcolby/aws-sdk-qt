4edbe65ae34080b99adfbcc3ebc382fed9726be9
Paul Colby <git@colby.id.au>
2015-07-25 19:53:52 +1000

    Update documentation and coverage reports

--- head/api/search/all_8.js
+++ head/api/search/all_8.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['label',['label',['../classSqsAddPermissionRequest.html#a348c7a324e531449333c8dea99f9e941',1,'SqsAddPermissionRequest::label()'],['../classSqsRemovePermissionRequest.html#ace81b6c665960730c4453df92a89aa5a',1,'SqsRemovePermissionRequest::label()']]]
+  ['label',['label',['../classSqsAddPermissionRequest.html#a348c7a324e531449333c8dea99f9e941',1,'SqsAddPermissionRequest::label()'],['../classSqsRemovePermissionRequest.html#ace81b6c665960730c4453df92a89aa5a',1,'SqsRemovePermissionRequest::label()']]],
+  ['listdeadlettersourcequeues',['listDeadLetterSourceQueues',['../classSqsClient.html#a4a47f3f7b4972f3809d02f69a7bb9fa8',1,'SqsClient::listDeadLetterSourceQueues(const SqsListDeadLetterSourceQueuesRequest &amp;request)'],['../classSqsClient.html#ada021b2c7d2c18d0466595f68800b4e1',1,'SqsClient::listDeadLetterSourceQueues(const QString &amp;queueUrl)']]],
+  ['listqueues',['listQueues',['../classSqsClient.html#aa153c311756c4f1b997c30bb1653014e',1,'SqsClient::listQueues(const SqsListQueuesRequest &amp;request)'],['../classSqsClient.html#a7218de2eeda5dc543f303ece9c1cb5eb',1,'SqsClient::listQueues(const QString &amp;queueNamePrefix=QString())']]]
 ];
--- head/internal/search/all_5.js
+++ head/internal/search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['getendpoint',['getEndpoint',['../classAwsEndpoint.html#a972e747803e29799e4b2ca6c211c7240',1,'AwsEndpoint']]]
+  ['getendpoint',['getEndpoint',['../classAwsEndpoint.html#a972e747803e29799e4b2ca6c211c7240',1,'AwsEndpoint']]],
+  ['getqueueurl',['getQueueUrl',['../classSqsClient.html#a2a5e2b31b7064619ff7c8ed79a629a00',1,'SqsClient::getQueueUrl(const SqsGetQueueUrlRequest &amp;request)'],['../classSqsClient.html#ae4cecbd6532edf1badd44c77fe086502',1,'SqsClient::getQueueUrl(const QString &amp;queueName, const QString &amp;queueOwnerAWSAccountId=QString())']]]
 ];
