24a543bf0d67ccb67b85d57c902d49cfca478a15
Paul Colby <git@colby.id.au>
2016-09-05 20:19:48 +1000

    Add initial schema and validation script

---
+++ scripts/validate-api-descriptions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while IFS= read -d '' -r FILENAME; do
+  echo "$FILENAME"
+  ERROR=`tv4 -s api-description.schema.json -j "$FILENAME" -v 2>&1 1>/dev/null`; RC=$?
+  [ -z "$ERROR" ] || { echo "$ERROR"; [ "$RC" -ne 0 ] || RC=1; }
+  [ "$RC" -eq 0 ] || exit $RC
+done < <(find ../descriptions -maxdepth 1 -name '*.json' -type f -print0)
