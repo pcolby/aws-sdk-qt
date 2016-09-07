a7277cb6c2557c3d3c046c988e6b8e85717b9d15
Paul Colby <git@colby.id.au>
2016-09-07 20:54:40 +1000

    Rename the descriptions directory for consistency

--- qrc/validate-api-descriptions.sh
+++ qrc/validate-api-descriptions.sh
@@ -5,4 +5,4 @@ while IFS= read -d '' -r FILENAME; do
   ERROR=`tv4 -s api-description.schema.json -j "$FILENAME" -v 2>&1 1>/dev/null`; RC=$?
   [ -z "$ERROR" ] || { echo "$ERROR"; [ "$RC" -ne 0 ] || RC=1; }
   [ "$RC" -eq 0 ] || exit $RC
-done < <(find descriptions -maxdepth 1 -name '*.json' -type f -print0)
+done < <(find api-descriptions -maxdepth 1 -name '*.json' -type f -print0)
