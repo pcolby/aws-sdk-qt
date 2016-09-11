91b3b1f81d77d5b90aec76084cf739962abde18e
Paul Colby <git@colby.id.au>
2016-09-11 12:16:08 +1000

    Fail validation if `find` found no files

--- qrc/validate-api-descriptions.sh
+++ qrc/validate-api-descriptions.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-SELF=`readlink --canonicalize "$0"`
-SELF_DIR=`dirname "$SELF"`
+SELF=`readlink --canonicalize "$0"` || exit
+SELF_DIR=`dirname "$SELF"` || exit
 DESC_DIR="$SELF_DIR/api-descriptions"
 DESC_SCHEMA="$SELF_DIR/api-description.schema.json"
 
+COUNT=0
 while IFS= read -d '' -r DESC_FILENAME; do
   basename "$DESC_FILENAME"
   ERROR=`tv4 -s "$DESC_SCHEMA" -j "$DESC_FILENAME" -v 2>&1 1>/dev/null`; RC=$?
   [ -z "$ERROR" ] || { echo "$ERROR"; [ "$RC" -ne 0 ] || RC=1; }
   [ "$RC" -eq 0 ] || exit $RC
+  COUNT=$(( $COUNT + 1 ))
 done < <(find "$DESC_DIR" -maxdepth 1 -name '*.json' -type f -print0)
+[ $COUNT -gt 0 ] # Ensure at least one file was found, in case `find` failed.
