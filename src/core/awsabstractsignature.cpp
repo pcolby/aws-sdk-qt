443fc77f6e5578ca53b048a5c45b02dcde0f5d5a
Paul Colby <git@colby.id.au>
2018-04-18 19:49:17 +1000

    Update all remaining QDocs for AwsAbstractSignature

--- src/core/awsabstractsignature.cpp
+++ src/core/awsabstractsignature.cpp
@@ -62,19 +62,22 @@ AwsAbstractSignature::~AwsAbstractSignature() {
 }
 
 /*!
- * @fn     void AwsAbstractSignature::sign() const
+ * \fn void AwsAbstractSignature::sign(const AwsAbstractCredentials &credentials,
+ *                                     const QNetworkAccessManager::Operation operation,
+ *                                     QNetworkRequest &request, const QByteArray &data) const
  *
- * @brief  Sign an AWS request.
+ * Sings the AWS network \a request using the supplied \a credentials.
  *
- * Note, \a credentials must be valid before calling this function.  So, for
+ * The network \a operation, and optional \c {POST/PUT} \a data may be used in
+ * the signing process, according to the implemented AWS signature version.
+ *
+ * \note \a credentials must be valid before calling this function.  So, for
  * example, if \a credentials has expired, and is refreshable, it is the
  * caller's responsibility to refresh the credentials before calling this
  * function.
  *
- * @param  operation     The network operation to sign \a request for.
- * @param  request       The network request to be signed.
- * @param  credentials   The credentials to use for signing.
- * @param  data          Optional POST / PUT data to sign \a request for.
+ * The signature if added to the \a request object itself (ie \a request will
+ * be modified by the call).
  */
 
 /*!
@@ -106,18 +109,14 @@ AwsAbstractSignaturePrivate::~AwsAbstractSignaturePrivate()
 }
 
 /*!
- * @brief  Create an AWS Signature canonical path.
+ * Returns an AWS Signature canonical path for \a url.
  *
  * This function simply returns the fully-URL-encoded path.  However, if the path
- * is empty, then a single '/' is returned, as is required by Amazon for both V2
+ * is empty, then a single \c / is returned, as is required by Amazon for both V2
  * and V4 signatures (and presumably other versions too).
  *
- * @param  url  URL from which to extract the path.
- *
- * @return An AWS Signature canonical path.
- *
- * @see    http://docs.aws.amazon.com/general/latest/gr/signature-version-2.html
- * @see    http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+ * \sa http://docs.aws.amazon.com/general/latest/gr/signature-version-2.html
+ * \sa http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
  */
 QString AwsAbstractSignaturePrivate::canonicalPath(const QUrl &url) const
 {
@@ -147,23 +146,19 @@ QString AwsAbstractSignaturePrivate::canonicalPath(const QUrl &url) const
 }
 
 /*!
- * @brief  Create an AWS Signature canonical query.
+ * Returns an AWS Signature canonical query string for \a query.
  *
  * This function returns an HTTP query string in Amazon's canonical form.  That is,
- * all query parameters are sorted by keys (**but not keys-then-values**), then
- * joined with `&` separators, in `key=value` pairs with both keys and values being
- * URL percent encoded.
+ * all query parameters are sorted by keys (\b {but not keys-then-values}), then
+ * joined with \c & separators, in \c {key=value} pairs with both keys and values
+ * being URL percent encoded.
  *
- * @note   The canonical form produced by this function is used by Amazon's later
+ * \note   The canonical form produced by this function is used by Amazon's later
  *         signature formats (versions 2, 3 and 4), but not their earlier formats
  *         (versions 0 and 1).
  *
- * @param  query  Query to encode the HTTP query string from.
- *
- * @return An AWS Signature canonical query string.
- *
- * @see    http://docs.aws.amazon.com/general/latest/gr/signature-version-2.html
- * @see    http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+ * \sa http://docs.aws.amazon.com/general/latest/gr/signature-version-2.html
+ * \sa http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
  */
 QByteArray AwsAbstractSignaturePrivate::canonicalQuery(const QUrlQuery &query) const
 {
@@ -180,15 +175,13 @@ QByteArray AwsAbstractSignaturePrivate::canonicalQuery(const QUrlQuery &query) c
 }
 
 /*!
- * @brief  Create an AWS Signature request method string.
+ * Returns the AWS Signature request method string for \a operation.
  *
  * This function simply converts QNetworkAccessManager operations (enum values)
  * to strings appropriate to use in AWS signatures.
  *
- * @param  operation  The network operation to convert to string.
- *
- * @return A string representation of \p operation, or an empty string if the
- *         operation is not recognised or otherwise unsupported.
+ * If \a operation is not a valid AWS network operation a null string will be
+ * returned.
  */
 QString AwsAbstractSignaturePrivate::httpMethod(const QNetworkAccessManager::Operation operation) const
 {
@@ -207,29 +200,21 @@ QString AwsAbstractSignaturePrivate::httpMethod(const QNetworkAccessManager::Ope
 }
 
 /*!
- * @brief   Set a query item, checking for existing values first.
+ * Sets the pair \a key = \a value in \a query, checking for existing values first.
  *
  * This function is a light wrapper around QUrlQuery::addQueryItem() that first
  * checks for existing values.  Existing values will not be overwritten, instead
  * if existing values are found, this function will simply check if the exsting
- * value matches the desired \a value, and if not, will return `false` and
- * optionally (according to \a warnOnNonIdenticalDuplicate) issue a qWarning().
+ * value matches the desired \a value, and if not, will return \c false and
+ * optionally (if \a warnOnNonIdenticalDuplicate is \c true) issue a qWarning().
  *
  * Typically, when setting something that must be a specific value, such as an
- * access key ID, \a warnOnNonIdenticalDuplicate would be `true`. However, when
+ * access key ID, \a warnOnNonIdenticalDuplicate would be \c true. However, when
  * setting query items as a fall-back default, such as a current timestamp,
- * \a warnOnNonIdenticalDuplicate would typically be set to `false`.
+ * \a warnOnNonIdenticalDuplicate would typically be set to \c false.
  *
- * @param   query  URL query to add the query item to.
- * @param   key    Query item key to add to \a query.
- * @param   value  Query item value to add to \a query.
- * @param   warnOnNonIdenticalDuplicate  If `true`, and an exisiting \a key
- *          value is found in \a query that has a value other than \a value,
- *          then a qWarning() is issued, otherwise the duplicate is silently
- *          ignored.
- *
- * @return  `true` if the query item was set successfully or was already set to
- *          the requested value previously, `false` otherwise.
+ * Returns \c true if the query item was set successfully or was already set to
+ * the requested value previously; \c false otherwise.
  */
 bool AwsAbstractSignaturePrivate::setQueryItem(QUrlQuery &query, const QString &key, const QString &value,
                                                const bool warnOnNonIdenticalDuplicate) const
@@ -248,14 +233,12 @@ bool AwsAbstractSignaturePrivate::setQueryItem(QUrlQuery &query, const QString &
 }
 
 /*!
- * @fn     int AwsAbstractSignature::version() const
+ * \fn int AwsAbstractSignature::version() const
  *
- * @brief  AWS Signature version implemented by this class.
+ * Returns the AWS Signature version implemented by this class.
  *
  * Derived classes must implement this function to report the version of
  * the AWS Signature implemented by the class.
- *
- * @return The AWS Signature version implemented by this class.
  */
 
 } // namespace Core
