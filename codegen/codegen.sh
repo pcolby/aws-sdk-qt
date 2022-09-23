25f01c9ceb4aabd22231e967c7475f0ee85e9865
Paul Colby <git@colby.id.au>
2022-09-23 13:28:18 +1000

    Add initial codegen.sh script

---
+++ codegen/codegen.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
+SELF_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+
+: "${MODELS_DIR:=${SELF_PATH%/*}/models}"
+: "${TEMPLATES_DIR:=${SELF_PATH%/*}/templates}"
+: "${OUTPUT_DIR:=${SELF_PATH%/*}/..}"
+
+function require {
+  local C
+  for c in "$@"; do
+    local -u V="${c//[^[:word:]]/_}";
+    if [ -v "$V" ]; then continue; fi
+    C=$(command -v "$c") || { echo "Required command not found: $c" >&2; exit 1; }
+    declare -gr "$V"="$C"
+  done
+}
+
+require git rm smithy-qt
+
+# \todo check for local changes
+# \todo remove existing generated files
+
+"$SMITHY_QT" --version
+"$SMITHY_QT" --models "$MODELS_DIR" --templates "$TEMPLATES_DIR" --output "$OUTPUT_DIR" "$@"
