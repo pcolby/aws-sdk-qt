274e9579db1ed28b4555985cdcf77f38533fd24f
Paul Colby <git@colby.id.au>
2022-10-01 23:44:26 +1000

    Switch to Doxygen + doxygen-awesome-css
    Because qdoc is a nightmare, and not intended for use outside of
    Qt's own modules anyway.

--- doc/global/style/README.md
+++
@@ -1,2 +0,0 @@
-
-* http://doc.qt.io/style/online.css
--- doc/src/qtaws-namespace.h
+++
@@ -1,7 +0,0 @@
-/*
-This namespace delcaration exists solely for the ../qtaws.qdocconf files.
-Without it, *.qdoc files included in qtaws.qdocconf would not see any code
-(classes, funtions, etc) in the QtAws namespace when using QDoc commands
-such as `\generatelist classes`.
-*/
-namespace QtAws {}
