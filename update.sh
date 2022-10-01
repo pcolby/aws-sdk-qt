03cbf719fe577a2812c5413eb4cc52e04706d62e
Paul Colby <git@colby.id.au>
2022-10-02 00:25:47 +1000

    Remove the old defunct update.sh script
    CI ations will take care of it soon.

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BUILDDIR=~/tmp/build/aws-sdk-qt-qmake-gcc-qt5101/doc
-
-rm -rf head
-mkdir head
-cp -a "$BUILDDIR/html/api" head/api
-cp -a "$BUILDDIR/html/int" head/internal
-#cp -a "$BUILDDIR/release/unit-test-all-coverage/" head/coverage
